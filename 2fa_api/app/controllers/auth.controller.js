a48d7c479f1b2f095b4c1ba1f55bbe44e3dfddf1
srirambtechit <srirambtecit@gmail.com>
2018-09-04 19:17:48 +0530

    Microservice full flow done

--- 2fa_api/app/controllers/auth.controller.js
+++ 2fa_api/app/controllers/auth.controller.js
@@ -1,7 +1,11 @@
 const Auth = require('../models/auth.model.js');
 const Ack = require('../models/ack.model.js');
+const threshold = 6;
+const delay = 10000; // 10 seconds
 
 exports.authReq = (req, res) => {
+
+    var authObject = { request: req, response: res, counter: 0 };
 
     if(!req.body) {
         return res.status(400).send({
@@ -9,48 +13,52 @@ exports.authReq = (req, res) => {
         });
     }
 
-    console.log(JSON.stringify(req.body));
-
     const auth = new Auth(req.body);
     auth.save()
     .then(data => {
       console.log('finding ack...');
-      console.log('req.body.mobileId : ' + req.body.mobileId);
-
-
-
-
-
+      authObject.timer = setInterval(findAck, delay, authObject);
+      console.log('Timer : ' + authObject.timer);
     }).catch(err => {
         res.status(500).send({
             message: err.message || "Some error occurred while creating the Auth"
         });
     });
-
-    // finding the acknowledgement for the particular transaction
-    console.log('mobileId : ' + req.body.mobileId);
-
 };
 
-function findAck(mobileId) {
-  Ack.findOne({mobileId: mobileId})
+function findAck(args) {
+  Ack.findOne({mobileId: args.request.body.mobileId})
   .then(ack => {
-      console.log('Ack then block');
-      if(!ack) {
-          return res.status(404).send({
-              message: "Auth not found with id " + req.body.mobileId
-          });
+      args.counter++;
+      console.log('counter : ' + args.counter);
+      console.log(JSON.stringify(ack));
+      if(ack != null || args.counter == threshold ) {
+          clearTimeout(args.timer);
+          args.ack = ack;
+          removeAuth(args);
       }
-      res.send(ack);
   }).catch(err => {
-      console.log('Ack catch block');
-      if(err.kind === 'ObjectId') {
-          return res.status(404).send({
-              message: "Auth not found with id " + req.body.mobileId
-          });
-      }
-      return res.status(500).send({
-          message: "Error retrieving auth with id " + req.body.mobileId
-      });
+      console.log(err.message || 'Error occurred during Ack finding');
+      return {};
   });
+}
+
+function removeAuth(args) {
+  Auth.deleteOne({mobileId: args.request.body.mobileId})
+  .then(auth => {
+    Ack.deleteOne({mobileId: args.request.body.mobileId})
+    .then(ack => {
+      return args.response.status(200).send(args.ack == null ? {} : args.ack);
+    }).catch(err => {
+      console.log(err.message || 'Error occurred during Ack deletion');
+      return {};
+    });
+  }).catch(err => {
+    console.log(err.message || 'Error occurred during Auth deletion');
+    return {};
+  });
+}
+
+exports.test = (req, res) => {
+  return res.status(200).send({message: "Testing"});
 }
--- 2fa_api/app/routes/auth.routes.js
+++ 2fa_api/app/routes/auth.routes.js
@@ -1,4 +1,5 @@
 module.exports = (app) => {
     const contoller = require('../controllers/auth.controller.js');
     app.post('/rbs2fa/api/auths', contoller.authReq);
+    app.get('/rbs2fa/api/test', contoller.test);
 }
